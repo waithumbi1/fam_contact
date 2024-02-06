a53e1b535c8a60b458393c7d2857ba7193cc006b
Stephen Waithumbi <stephenwaithumbi11@gmail.com>
2024-02-06 03:11:09 +0300

    Settings updated

--- my_casino_club/settings.py
+++ my_casino_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p8#en29s=w9*05qf%%itn*touvf#hyl)10u_zu9+wz5oq*eg3q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
